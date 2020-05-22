21267a8dbe0c70a5af1b8baef08342df2c8d257b
Me <me@Me-MacBook-Air.local>
2020-05-22 18:16:15 +0100

    Updates

---
+++ precache-manifest.b150a63301c2eeee32b7966120913ec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2ae81076e3b254780b8427f7adbcbc",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "62ae5ebb7fc3b1e6b692",
+    "url": "/react-monsters-rolodex/static/css/main.06f8edf8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ae5ebb7fc3b1e6b692",
+    "url": "/react-monsters-rolodex/static/js/main.241bf6be.chunk.js"
+  },
+  {
+    "revision": "84d462a1ad20fdb403f4",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.6eda5f3f.js"
+  }
+]);
